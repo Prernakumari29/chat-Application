3c215ced485d7698cf77a81fb57b2e8091357739
Prernakumari29 <prernajaiswal646@gmail.com>
2026-08-04 08:33:02 +0530

    url updated

--- frontend/src/services/Api.jsx
+++ frontend/src/services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://chat-application-0kcn.onrender.com/api",
     withCredentials:true
 });
 
