11428655ae6aae22dab447fef41ece135c82cd50
Prernakumari29 <prernajaiswal646@gmail.com>
2026-08-04 08:43:04 +0530

    url update

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,10 @@ app.use(express.urlencoded({extended:true}))
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:[
+       "http://localhost:5173",
+      "https://chat-application-virid-eight.vercel.app/"
+    ],
     credentials: true,
   })
 );
